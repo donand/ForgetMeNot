daeacf2bb04771bf6d312fd1c7dc1a718c74eb57
SaraVeterini <saraveterini@gmail.com>
2015-06-27 18:24:51 +0200

    primo commit

---
+++ app/src/main/java/com/forgetmenot/forgetmenot/CustomListPiante.java
@@ -0,0 +1,142 @@
+package com.forgetmenot.forgetmenot;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Sara on 27/06/2015.
+ */
+public class CustomListPiante extends BaseAdapter {
+    private final Activity context;
+    LayoutInflater inflater;
+    private final JSONArray elencoPiante;
+
+
+    public CustomListPiante(Activity context, JSONArray elenco) {
+        this.inflater = LayoutInflater.from(context);
+        this.context = context;
+        this.elencoPiante=elenco;
+    }
+    public int getCount() {
+        return elencoPiante.length();
+    }
+
+    public JSONObject getItem(int position) {
+        try {
+            return elencoPiante.getJSONObject(position);
+        }
+        catch(JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public long getItemId(int position) {
+
+        try {
+            return elencoPiante.getJSONObject(position).getInt("ID");
+        }
+        catch(JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = this.inflater.inflate(R.layout.list_piante_utente_item,
+                    parent, false);
+            holder.nomePianta = (TextView) convertView.findViewById(R.id.nomePianta);
+            holder.la=(ProgressBar)convertView.findViewById(R.id.acquaProgress);
+            holder.lf=(ProgressBar)convertView.findViewById(R.id.fertilizzanteProgress);
+            holder.stato=(ImageView)convertView.findViewById(R.id.statoPianta);
+            holder.immaginePianta=(ImageView)convertView.findViewById(R.id.immaginePianta);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        try {
+            JSONObject o = elencoPiante.getJSONObject(position);
+            System.out.println(o.toString());
+            holder.nomePianta.setText(o.getString("nomeAssegnato"));
+            holder.livelloAcqua=o.getInt("livelloAcqua");
+            holder.livelloFertilizzante=o.getInt("livelloConcimazione");
+
+            holder.la.setProgress(holder.livelloAcqua);
+            holder.lf.setProgress(holder.livelloFertilizzante);
+
+            //se si verifica vuol dire che la pianta sta male.
+            if(holder.livelloFertilizzante<=0 || holder.livelloAcqua<=0){
+                holder.stato.setImageResource(R.drawable.bad);
+            }
+            else holder.stato.setImageResource(R.drawable.happy);
+
+            Picasso.with(context)
+                    .load(o.getString("immagine")).into(holder.immaginePianta);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return convertView;
+    }
+
+    private class ViewHolder {
+        ImageView stato;
+        ProgressBar la;
+        ProgressBar lf;
+        TextView nomePianta;
+        ImageView immaginePianta;
+        int livelloAcqua;
+        int livelloFertilizzante;
+    }
+
+}
+    /*
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView= inflater.inflate(R.layout.list_piante_utente_item, null, true);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.immaginePianta);
+        TextView nome = (TextView) rowView.findViewById(R.id.nomePianta);
+
+        try{
+            nome.setText(elencoPiante.getJSONObject(position).getString("nome"));
+            Picasso.with(context)
+                    .load(elencoPiante.getJSONObject(position).getString("immagine")).into(imageView);
+            int livelloAcqua=elencoPiante.getJSONObject(position).getInt("livelloAcqua");
+            int livelloFertilizzante=elencoPiante.getJSONObject(position).getInt("livelloFertilizzante");
+            ProgressBar la=(ProgressBar)rowView.findViewById(R.id.acquaProgress);
+            la.setProgress(livelloAcqua);
+            ProgressBar lf=(ProgressBar)rowView.findViewById(R.id.fertilizzanteProgress);
+            lf.setProgress(livelloFertilizzante);
+
+             //controlli sul livello acqua e impostazione stato. ora imposto sempre felice
+            ImageView stato=(ImageView)rowView.findViewById(R.id.statoPianta);
+            stato.setImageResource(R.drawable.happy);
+            System.out.println(nome.getText());
+        }
+        catch(JSONException e){
+            e.printStackTrace();
+        }
+        return rowView;
+    }
+}*/
--- app/src/main/java/com/forgetmenot/forgetmenot/MainActivity.java
+++ app/src/main/java/com/forgetmenot/forgetmenot/MainActivity.java
@@ -1,26 +1,55 @@
 package com.forgetmenot.forgetmenot;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.SearchView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements TaskCallbackElenco {
     String prova ;
+    JSONArray elencoPiante=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //chiamo il task che prende l'elenco di piante dell'utente dal server. ora provo con un utente a caso
+        try {
+            String ricerca = "http://forgetmenot.ddns.net/ForgetMeNot/ElencoPianteUtente?utente=1";
+            GetElencoPianteUtente task = new GetElencoPianteUtente(ricerca, this, this.getApplicationContext());
+            task.execute();
+        } catch (Exception e) {
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) (menu.findItem(R.id.search)).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
         return true;
     }
 
@@ -39,9 +68,66 @@ public class MainActivity extends ActionBarActivity {
         if(id == R.id.luce){
             Intent i = new Intent(MainActivity.this, VerificaLuce.class);
             this.startActivity(i);
+        }
 
+        if(id==R.id.search){
+            onSearchRequested();
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+    public void done(String result){
+        try{
+            TextView nome=(TextView)findViewById(R.id.nomePianta);
+            if(result.equals(null) || result.equals("")){
+                nome.setVisibility(View.VISIBLE);
+                nome.setText("Non hai nessuna pianta.");
+            }
+            elencoPiante=new JSONArray(result);
+
+            impostaMessaggioIniziale(elencoPiante);
+
+            //listview di activity_main
+            ListView listaPiante=(ListView)findViewById(R.id.piante);
+            CustomListPiante adapter = new CustomListPiante(MainActivity.this, elencoPiante);
+            listaPiante.setAdapter(adapter);
+
+            /*String nomeR="Nome";
+            String immagineR="http://www.drogbaster.it/immagini-3d/album/slides/immagini-tridimensionali%20(5).jpg";*/
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void impostaMessaggioIniziale(JSONArray result){
+        boolean bad=false;
+        for(int i=0; i<result.length(); i++){
+            try{
+                JSONObject o=result.getJSONObject(i);
+                int livelloAcqua=o.getInt("livelloAcqua");
+                int livelloFertilizzante=o.getInt("livelloConcimazione");
+                if(livelloAcqua<=5 || livelloFertilizzante<=5){
+                    bad=true;
+                }
+            }
+            catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        TextView messaggioIniziale=(TextView)findViewById(R.id.messaggio);
+
+        if(bad){
+            messaggioIniziale.setText("Attenzione. Qualcuna delle tue piante ha bisogno di cure.");
+            messaggioIniziale.setTextColor(Color.RED);
+            messaggioIniziale.setGravity(Gravity.CENTER);
+        }
+        else{
+            messaggioIniziale.setText("Le tue piante stanno bene.");
+            messaggioIniziale.setTextColor(Color.GREEN);
+            messaggioIniziale.setGravity(Gravity.CENTER);
+        }
+    }
+
 }
